1b768208b36cd52d16e04efc71d746a203ae7b10
Hadixp <hadi.b.2002@gmail.com>
2024-08-06 01:40:23 +0430

    feat: empty error

--- main.js
+++ main.js
@@ -1,35 +1,38 @@
 // my Objects
 
 const User1 = {
-    name : 'hadi',
-    lastName : 'bahadori',
-    age : 22
-}
+  name: "hadi",
+  lastName: "bahadori",
+  age: 22,
+};
 const User2 = {
-    name : 'alireza',
-    lastName : 'noshad',
-    age : 22
-}
+  name: "alireza",
+  lastName: "noshad",
+  age: 22,
+};
 const User3 = {
-    name : 'negar',
-    lastName : 'tamimi',
-    age : 20
-}
+  name: "negar",
+  lastName: "tamimi",
+  age: 20,
+};
 const User4 = {
-    name : 'mohammad',
-    lastName : 'kohan',
-    age : 21
-}
+  name: "mohammad",
+  lastName: "kohan",
+  age: 21,
+};
 const User5 = {
-    name : 'amirhossein',
-    lastName : 'azad',
-    age : 21
-}
+  name: "amirhossein",
+  lastName: "azad",
+  age: 21,
+};
 
 // the below function get a Object name as a parameter and return a text
 
-function getUser(user){
-    return `${user.name} ${user.lastName} is ${user.age}`;
+function getUser(user) {
+  if (user == undefined) {
+    throw new Error("your parameter can not be empty");
+  }
+  return `${user.name} ${user.lastName} is ${user.age}`;
 }
 
 // now we log the function
